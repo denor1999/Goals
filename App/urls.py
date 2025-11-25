5eab81ab76a1de5ad0f9d91778ebaa67d27e3d49
denor1999 <ozhgixinv@list.ru>
2025-11-25 10:53:50 +0300

    Update URL

--- App/urls.py
+++ App/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('tasks/', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('logout/', views.logout_view, name='logout'),
-    path('goal/<int:goal_id>/', views.goal_detail, name='goal_detail'),
+    path('tasks/<int:goal_id>/', views.goal_detail, name='goal_detail'),
     path('goal/add/', views.add_goal, name='add_goal'),
     path('goal/<int:goal_id>/add-step/', views.add_step, name='add_step'),
     path('goal/<int:goal_id>/complete/', views.complete_goal, name='complete_goal'),
